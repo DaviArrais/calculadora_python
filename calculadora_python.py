ded8fd44ef555474b92554d4a55a06ccfd4d2fdb
Davi Arrais <daviarrais@hotmail.com>
2021-11-15 11:27:31 -0300

    Update calculadora_python.py

--- calculadora_python.py
+++ calculadora_python.py
@@ -54,7 +54,21 @@ botao_a=Button(Frame_numero, text="<",command=percentual)
 botao_a.place(x=223,y=174, width=90, height=38) 
 
 
+
 #Janela IMC
+def calcular_imc():
+    pass
+
 LabelFrame_imc=LabelFrame(janela,text="IMC",borderwidth=1, relief="solid")
 LabelFrame_imc.place(x=510,y=20, width=470, height=460)
+Frame_imc=Frame(LabelFrame_imc,borderwidth=1, relief="solid").place(x=20,y=20, width=430, height=100)
+
+Label(LabelFrame_imc,text="Peso", font= "20", anchor=W).place(x=80, y=150, width=50, height=50)
+peso=Entry(LabelFrame_imc).place(x=140, y=153, width=100, height=50)
+Label(LabelFrame_imc,text="Altura", font= "20", anchor=W).place(x=80, y=230, width=50, height=80)
+altura=Entry(LabelFrame_imc).place(x=140, y=243, width=100, height=50)
+Button(LabelFrame_imc,text="Calcular IMC", command=calcular_imc).place(x=140, y=330, width=100, height=50)
+
+Frame_imc_indice=Frame(LabelFrame_imc, borderwidth=1, relief="solid" ).place(x=290,y=313, width=120, height=80)
+
 janela.mainloop()
