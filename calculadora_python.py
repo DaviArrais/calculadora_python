1afeb8e1e768246582a8f7f8232e93cb73b455f0
Davi Arrais <daviarrais@hotmail.com>
2021-11-20 15:55:07 -0300

    versão 1.3

--- calculadora_python.py
+++ calculadora_python.py
@@ -275,45 +275,44 @@ botao_v.place(x=113,y=174, width=90, height=38)
 botao_a=Button(Frame_numero, text="<",command=apagar_ultimo)
 botao_a.place(x=223,y=174, width=90, height=38) 
 
-
-
 #Janela IMC
-
-
 #textos utilizados
 t1="Informe o peso e a altura!!!"
-
 #variáveis
 p = StringVar()
 a = StringVar()
 imc = StringVar()
+indice=0
 texto= StringVar()
 texto.set(t1)
 
 def calcular_imc():
     peso=int(p.get())
     altura=float(a.get())
-    #i=peso * (altura**2
-    imc.set(peso / (altura**2))
-    
+    indice=peso / (altura**2)
+    if indice<=18.5:
+        t1='Você está abaixo do peso!'
+    elif (indice>18.5) and (indice<=25):
+        t1='Você está no peso ideal!'
+    elif (indice>25) and (indice<=30):
+        t1='Você está com sobrepeso!'
+    elif (indice>30) and (indice<=40):
+        t1='Você está obeso!'
+    else:
+        t1='Você está com obesidade grave!'
+    imc.set(round(indice,2))  
+    texto.set(t1)
 
 LabelFrame_imc=LabelFrame(janela,text="IMC",borderwidth=1, relief="solid")
 LabelFrame_imc.place(x=510,y=20, width=470, height=460)
 Frame_imc=Frame(LabelFrame_imc,borderwidth=1, relief="solid").place(x=20,y=20, width=430, height=100)
-
 Label(LabelFrame_imc,text="Peso", font= "20", anchor=W).place(x=80, y=150, width=50, height=50)
 peso=Entry(LabelFrame_imc, textvariable=p).place(x=140, y=153, width=100, height=50)
-
 Label(LabelFrame_imc,text="Altura", font= "20", anchor=W).place(x=80, y=230, width=50, height=80)
 altura=Entry(LabelFrame_imc, textvariable=a).place(x=140, y=243, width=100, height=50)
-
 Button(LabelFrame_imc,text="Calcular IMC", command=calcular_imc).place(x=140, y=330, width=100, height=50)
-
-Label(LabelFrame_imc, textvariable=texto).place(x=50, y=50, width=300, height=40)
-
-#Frame_imc_indice=Frame(LabelFrame_imc, borderwidth=1, relief="solid" ).place(x=290,y=313, width=120, height=80)
-#valor_imc=Label(Frame_imc_indice, text="Valor imc", textvariable=imc)
-#valor_imc.place(x=20, y=20, width=20, height=20)
-
+Label(LabelFrame_imc, textvariable=texto).place(x=40, y=40, width=380, height=60)
+Frame_imc_indice=LabelFrame(LabelFrame_imc, borderwidth=1, relief="solid" ).place(x=290,y=313, width=120, height=80)
+Label(LabelFrame_imc, textvariable=imc).place(x=300, y=330, width=100, height=50)
 
 janela.mainloop()
