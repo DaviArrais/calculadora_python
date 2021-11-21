755733f0f6b88d8f85fa42fe6d65b8e914d8d6bb
Davi Arrais <daviarrais@hotmail.com>
2021-11-21 15:47:09 -0300

    ajuste de design

--- calculadora_python.py
+++ calculadora_python.py
@@ -230,7 +230,7 @@ Frame_calculadora=Frame(LabelFrame_calculadora,borderwidth=1, relief="solid")
 Frame_calculadora.place(x=20,y=20, width=430, height=100)
 
 l=Label(Frame_calculadora, textvariable=n1).pack()
-t=Entry(Frame_calculadora, textvariable=var, justify=RIGHT).pack()
+t=Entry(Frame_calculadora, textvariable=var, justify=RIGHT,font=("Arial",25)).place(x=20,y=20, width=390, height=60)
 
 botao_percentual=Button(LabelFrame_calculadora, text="%",command=percentual)
 botao_percentual.place(x=25,y=140, width=90, height=44) 
@@ -311,8 +311,8 @@ peso=Entry(LabelFrame_imc, textvariable=p).place(x=140, y=153, width=100, height
 Label(LabelFrame_imc,text="Altura", font= "20", anchor=W).place(x=80, y=230, width=50, height=80)
 altura=Entry(LabelFrame_imc, textvariable=a).place(x=140, y=243, width=100, height=50)
 Button(LabelFrame_imc,text="Calcular IMC", command=calcular_imc).place(x=140, y=330, width=100, height=50)
-Label(LabelFrame_imc, textvariable=texto).place(x=40, y=40, width=380, height=60)
+Label(LabelFrame_imc, textvariable=texto,font=("Arial",20)).place(x=40, y=40, width=380, height=60)
 Frame_imc_indice=LabelFrame(LabelFrame_imc, borderwidth=1, relief="solid" ).place(x=290,y=313, width=120, height=80)
-Label(LabelFrame_imc, textvariable=imc).place(x=300, y=330, width=100, height=50)
+Label(LabelFrame_imc, textvariable=imc,font=("Arial",25)).place(x=300, y=330, width=100, height=50)
 
 janela.mainloop()
