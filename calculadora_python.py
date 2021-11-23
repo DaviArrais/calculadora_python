282fec4a7cd40284999320cc0b70ae001facd28f
Davi Arrais <daviarrais@hotmail.com>
2021-11-22 23:24:03 -0300

    Criação das abas e inclusão de comentários

--- calculadora_python.py
+++ calculadora_python.py
@@ -1,28 +1,26 @@
+#Importe de bibliotecas
 from tkinter import *
 from tkinter import ttk
 from typing import ValuesView
+#Declaração da janela
 janela=Tk()
 janela.title("Calculadora Tkinter - Matemática - IMC")
-janela.geometry("1000x500")
-
+janela.geometry("510x500")
+#Declaração das abas
+aba=ttk.Notebook(janela)
+aba.place(x=20,y=20, width=470, height=460)
 #Variavel para receber comandos
 lista_comandos=[]
 var=StringVar()
-
 n1=StringVar()
-x=0
-y=0
 numerador=0
-denominador=0
 op=""
-#Funções
+#Funções associadas aos botões
 def percentual():
     var.set("%")
     print(lista_comandos)
     print(numerador)
-
 def num1():
-    #var.set(1)
     lista_comandos.append(1)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -31,10 +29,7 @@ def num1():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-    
-        
 def num2():
-    #var.set(2)
     lista_comandos.append(2)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -43,9 +38,7 @@ def num2():
     numerador=int(operador)
     numerador= var.get() 
     print(numerador)
-
 def num3():
-    #var.set(3)
     lista_comandos.append(3)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -54,9 +47,7 @@ def num3():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-        
 def num4():
-    #var.set(4)
     lista_comandos.append(4)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -65,9 +56,7 @@ def num4():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-    
 def num5():
-    #var.set(5)
     lista_comandos.append(5)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -76,9 +65,7 @@ def num5():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-        
 def num6():
-    #var.set(6)
     lista_comandos.append(6)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -87,9 +74,7 @@ def num6():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-
 def num7():
-    #var.set(7)
     lista_comandos.append(7)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -98,9 +83,7 @@ def num7():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-        
 def num8():
-    #var.set(8)
     lista_comandos.append(8)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -109,9 +92,7 @@ def num8():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-    
 def num9():
-    #var.set(9)
     lista_comandos.append(9)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -120,9 +101,7 @@ def num9():
     numerador= var.get() 
     print(lista_comandos)
     print(numerador)
-        
 def num0():
-    #var.set(0)
     lista_comandos.append(0)
     operador=""
     for n in range(0, len(lista_comandos)):
@@ -131,7 +110,6 @@ def num0():
     numerador=int(operador)
     print(lista_comandos)
     print(numerador)
-
 def igual():
     retorno=0
     x1=int(n1.get())
@@ -151,20 +129,17 @@ def igual():
     var.set(retorno)
     print(lista_comandos)
     print(retorno)
-
 def apagar_ultimo():
     var.set("<")
     lista_comandos.pop()
     print(lista_comandos)
     print(numerador)
-
 def limpar():
     var.set("")
     n1.set("")
     for x in range(0,len(lista_comandos)):
         lista_comandos.pop()
-    print(lista_comandos)
-    
+    print(lista_comandos)   
 def quadrado():
     n=var.get()
     n1.set(n)
@@ -174,7 +149,6 @@ def quadrado():
     for x in range(0,len(lista_comandos)):
         lista_comandos.pop()
     print(lista_comandos)
-
 def divisao():
     n=var.get()
     n1.set(n)
@@ -184,7 +158,6 @@ def divisao():
     for x in range(0,len(lista_comandos)):
         lista_comandos.pop()
     print(lista_comandos)
-
 def multiplicacao():
     n=var.get()
     n1.set(n)
@@ -194,7 +167,6 @@ def multiplicacao():
     for x in range(0,len(lista_comandos)):
         lista_comandos.pop()
     print(lista_comandos)
-
 def subtracao():
     n=var.get()
     n1.set(n)
@@ -203,9 +175,7 @@ def subtracao():
     var.set("-")
     for x in range(0,len(lista_comandos)):
         lista_comandos.pop()
-    
     print(lista_comandos)
-
 def soma():
     n=var.get()
     n1.set(n)
@@ -216,22 +186,17 @@ def soma():
         lista_comandos.pop()
     print(lista_comandos)
     print(numerador)
-
     print(lista_comandos)
-
 def virgula():
     var.set(",")
     print(lista_comandos)
-
-#Janela Matematica
-LabelFrame_calculadora=LabelFrame(janela,text="MATEMÁTICA",borderwidth=1, relief="solid")
-LabelFrame_calculadora.place(x=20,y=20, width=470, height=460)
+#Aba calculadora matemática com os elementos
+LabelFrame_calculadora=LabelFrame(aba)
+aba.add(LabelFrame_calculadora, text="MATEMÁTICA")
 Frame_calculadora=Frame(LabelFrame_calculadora,borderwidth=1, relief="solid")
 Frame_calculadora.place(x=20,y=20, width=430, height=100)
-
 l=Label(Frame_calculadora, textvariable=n1).pack()
 t=Entry(Frame_calculadora, textvariable=var, justify=RIGHT,font=("Arial",25)).place(x=20,y=20, width=390, height=60)
-
 botao_percentual=Button(LabelFrame_calculadora, text="%",command=percentual)
 botao_percentual.place(x=25,y=140, width=90, height=44) 
 botao_limpar=Button(LabelFrame_calculadora, text="C",command=limpar)
@@ -274,18 +239,16 @@ botao_v=Button(Frame_numero, text=",",command=virgula)
 botao_v.place(x=113,y=174, width=90, height=38) 
 botao_a=Button(Frame_numero, text="<",command=apagar_ultimo)
 botao_a.place(x=223,y=174, width=90, height=38) 
-
-#Janela IMC
-#textos utilizados
+#Aba IMC
+#variáveis utilizados
 t1="Informe o peso e a altura!!!"
-#variáveis
 p = StringVar()
 a = StringVar()
 imc = StringVar()
 indice=0
 texto= StringVar()
 texto.set(t1)
-
+#Função associada ao botão
 def calcular_imc():
     peso=int(p.get())
     altura=float(a.get())
@@ -302,9 +265,9 @@ def calcular_imc():
         t1='Você está com obesidade grave!'
     imc.set(round(indice,2))  
     texto.set(t1)
-
-LabelFrame_imc=LabelFrame(janela,text="IMC",borderwidth=1, relief="solid")
-LabelFrame_imc.place(x=510,y=20, width=470, height=460)
+#Aba calculadora IMC com os elementos
+LabelFrame_imc=LabelFrame(aba)
+aba.add(LabelFrame_imc, text="IMC")
 Frame_imc=Frame(LabelFrame_imc,borderwidth=1, relief="solid").place(x=20,y=20, width=430, height=100)
 Label(LabelFrame_imc,text="Peso", font= "20", anchor=W).place(x=80, y=150, width=50, height=50)
 peso=Entry(LabelFrame_imc, textvariable=p).place(x=140, y=153, width=100, height=50)
@@ -314,5 +277,5 @@ Button(LabelFrame_imc,text="Calcular IMC", command=calcular_imc).place(x=140, y=
 Label(LabelFrame_imc, textvariable=texto,font=("Arial",20)).place(x=40, y=40, width=380, height=60)
 Frame_imc_indice=LabelFrame(LabelFrame_imc, borderwidth=1, relief="solid" ).place(x=290,y=313, width=120, height=80)
 Label(LabelFrame_imc, textvariable=imc,font=("Arial",25)).place(x=300, y=330, width=100, height=50)
-
+#Comando para atualização de janela
 janela.mainloop()
--- teste_lista.py
+++
@@ -1,5 +0,0 @@
-lista_comandos=[8,9,7,6]
-operador=""
-for n in range(0, len(lista_comandos)):
-    operador=operador+str(lista_comandos[n])
-print(operador)
